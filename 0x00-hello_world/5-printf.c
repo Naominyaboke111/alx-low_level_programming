2a622532f90fc96371c1ebdae839bd000e59d720
Naominyaboke111 <naominyaboke111@gmail.com>
2023-03-16 13:13:19 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- *
- *
+ *main- C program that prints a string followed by a line using printf function
+ *return: 0(code successful)
  */
 int main(void)
-
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return(0);
+}
