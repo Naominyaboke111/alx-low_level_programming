4c8d8cfb907296a972294901c6ab67ff82cc54d0
Naominyaboke111 <naominyaboke111@gmail.com>
2023-03-16 12:46:30 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  *program uses puts function 
  */
  int main (void){
-	puts(\"Programming is like building a multilingual puzzle\");
+	puts("\"Programming is like building a multilingual puzzle"\");
 	return (0);
  }
