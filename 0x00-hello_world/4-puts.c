370d7178e3463177e521a856c06cd8aeadbe5f68
Naominyaboke111 <naominyaboke111@gmail.com>
2023-03-16 12:55:57 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *C program that prints line ,followed by a line 
- *program uses puts function 
+ *C program that prints line followed by a line
+ *program uses puts function
  */
  int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
- }
+}
