afae2ffe0fd5c589f718439ec6a8e437bcfff019
Naominyaboke111 <naominyaboke111@gmail.com>
2023-03-17 09:16:04 -0700

    Task 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * main -  program will assign a random number to the variable n each time it is executed.
- *if the last digit of n is greater than 5: the string and is greater than 5
- *if the last digit of n is 0: the string and is 0
- *if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ * main -  program will assign a random number to the variable n each time it is executed.n 
+ *
+ * could be greater than 5,less than 6 or 0
+ *
  * Return: 0 (Code Successful).
  */
 int main(void)
